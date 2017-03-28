2891c3a7bdb49845e23401b3aa855dff3d043130
tkell9 <tkell9@hotmail.com>
2017-03-28 16:13:27 +1000

    Second Commit - Greyboxing updated. Prototype game in progress.

---
+++ Assets/2Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+
+    //Take reference of the player.
+    //public Transform lookAt;
+
+    //This puts the camera at an offset away from the player.
+    private Vector3 offset = new Vector3(0f, 4f, -10f);
+
+    public Transform FollowCamera;
+    public Transform Player;
+
+    private void Start()
+    {
+
+    }
+
+    //Updated so now the camera follows the player on the X axis only
+    private void Update()
+    {
+        FollowCamera.position = new Vector3(Player.position.x, 0, 0) + offset;
+    }
+
+
+
+}
---
+++ Assets/2Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    //Sets target for enemy to move towards, always the player unless decoy is used.
+    public Transform target;
+    public float move_speed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime * move_speed);
+
+
+	}
+}
