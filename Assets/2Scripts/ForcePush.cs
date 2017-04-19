69ef2236b3a11d95a5d9ee5e3e819cc3f56a7992
SAE\tkell <tkell9@hotmail.com>
2017-04-19 13:41:48 +1000

    Worked on the force push effect.

---
+++ Assets/2Scripts/ForcePush.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ForcePush : MonoBehaviour {
+
+    public Transform playerLocation;
+    public GameObject sphere;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetButtonDown("Space"))
+        {
+
+            Instantiate(sphere, transform.position, transform.rotation);
+        }	    
+
+
+	}
+}
---
+++ Assets/Sphere.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour {
+
+    public Transform scale;
+    public bool isActive = false;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+
+	}
+}
