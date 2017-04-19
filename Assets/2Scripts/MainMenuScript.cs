8111feecc884a878e925f01ca882b09c4f57e6d5
tkell9 <tkell9@hotmail.com>
2017-04-20 01:25:40 +1000

    Menu changes. Buttons are now functional and display character change scripts have been started.

--- Assets/2Scripts/MainMenuScript.cs
+++ Assets/2Scripts/MainMenuScript.cs
@@ -3,25 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuScript : MonoBehaviour {
+public class MainMenuScript : MonoBehaviour
+{
+    private int prefabToShow;
+    public GameObject[] buildings;
+    public Transform spawnPoint;
+    public GameObject trumpIdle;
+    public GameObject kanyeIdle;
 
-  //  int levelToLoad;
+
+    // Use this for initialization
+    void Start()
+    {
+          switch (prefabToShow)
+        {
+            case 1:
+                Instantiate(kanyeIdle, spawnPoint);
+                break;
+
+            case 2:
+                Instantiate(trumpIdle, spawnPoint);
+                break;
+        }
+
+    
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
 
 
-	// Use this for initialization
-	void Start () {
+    //Switch statement that changes the idle character on the main menu
+   
 
+    public void kanye()
+    {
+        //Will set the menu prefab to kanye and will set the border to active.
+        prefabToShow = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-
-	}
+    public void trump()
+    {
+        //Will set the menu prefab to Trump and will set the border to active.
+        prefabToShow = 2;
+        SceneManager.LoadScene("MainMenu");
+    }
 
     public void LoadLevel(string _SceneName)
     {
+        //This allows me to place the script on any button and then set
+        //the level in the editor.
         SceneManager.LoadScene(_SceneName);
+    }
+
+    public void exitGame()
+    {
+        //Exit the game when clicking the exit button.
+        Application.Quit();
     }
 }
---
+++ Assets/PauseButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseButton : MonoBehaviour {
+
+
+    private Sprite pauseMenu;
+
+	// Use this for initialization
+	void Start ()
+    {
+        pauseMenu = GetComponent<Sprite>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void pauseGame()
+    {
+        Time.timeScale = 0;
+        GetComponent<pauseMenu>.enabled = true;
+    }
+
+    public void resumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
