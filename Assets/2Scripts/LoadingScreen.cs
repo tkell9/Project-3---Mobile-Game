ab52128860dd99a7f9849cdaba9e7248d4f17932
tkell9 <tkell9@hotmail.com>
2017-04-28 21:50:16 +1000

    no message

---
+++ Assets/2Scripts/LoadingScreen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingScreen : MonoBehaviour {
+
+    public Texture2D controllerScreen;
+    static LoadingScreen instance1;
+
+
+    private void Awake()
+    {
+        if (instance1)
+        {
+            Destroy(gameObject);
+          //  hide();
+            return;
+        }
+        instance1 = this;
+        gameObject.AddComponent<GUITexture>().enabled = false;
+       // GetComponent<Texture2D>().controllerScreen = controllerScreen;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
--- Assets/2Scripts/MainMenuScript.cs
+++ Assets/2Scripts/MainMenuScript.cs
@@ -11,23 +11,19 @@ public class MainMenuScript : MonoBehaviour
     public GameObject trumpIdle;
     public GameObject kanyeIdle;
 
+    private GameObject playOptions;
+    public bool tutorialPlayed;
+
+
+
 
     // Use this for initialization
     void Start()
     {
-          switch (prefabToShow)
-        {
-            case 1:
-                Instantiate(kanyeIdle, spawnPoint);
-                break;
-
-            case 2:
-                Instantiate(trumpIdle, spawnPoint);
-                break;
-        }
-
-    
-}
+        playOptions = GameObject.FindGameObjectWithTag("PlayOptions");
+        playOptions.gameObject.SetActive(false);
+        tutorialComplete = false;
+    }
 
     // Update is called once per frame
     void Update()
@@ -45,6 +41,24 @@ public class MainMenuScript : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
     }
 
+    public void playButton()
+    {
+        playOptions.gameObject.SetActive(true);
+    }
+
+    public void tutorial()
+    {
+        playOptions.gameObject.SetActive(false);
+        SceneManager.LoadScene("TutorialLevel");
+        tutorialComplete = true;
+    }
+
+    public void play()
+    {
+        playOptions.gameObject.SetActive(false);
+        SceneManager.LoadScene("Level1");
+    }
+
     public void trump()
     {
         //Will set the menu prefab to Trump and will set the border to active.
@@ -52,16 +66,44 @@ public class MainMenuScript : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
     }
 
-    public void LoadLevel(string _SceneName)
+    public void tutorialComplete()
     {
-        //This allows me to place the script on any button and then set
-        //the level in the editor.
-        SceneManager.LoadScene(_SceneName);
+        if (tutorialComplete)
+        {
+
+        }
     }
+  
+    
+    //This allows me to place the script on any button and then set
+    // the level in the editor. I dont need this function anymore but it will be usefull
+    // in future projects.
+    // public void LoadLevel(string _SceneName)
+    //  {
+    //     
+    //      SceneManager.LoadScene(_SceneName);
+    //  }
+
+
 
     public void exitGame()
     {
         //Exit the game when clicking the exit button.
         Application.Quit();
     }
+     
+    
+    
+
+
+    /*switch (prefabToShow)
+        {
+            case 1:
+                Instantiate(kanyeIdle, spawnPoint);
+                break;
+
+            case 2:
+                Instantiate(trumpIdle, spawnPoint);
+                break;
+        }*/
 }
