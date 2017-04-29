4404bddfef2ec8867093020bc9312cbf84a53815
tkell9 <tkell9@hotmail.com>
2017-04-30 00:49:05 +1000

    no message

--- Assets/2Scripts/EnemyController.cs
+++ Assets/2Scripts/EnemyController.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour {
 
     //Sets target for enemy to move towards, always the player unless decoy is used.
-    public Transform target;
-    public float move_speed;
+    //public Transform target;
+    //public float move_speed;
 
+    private int levelOfDifficulty;
+    private int enemyToSpawn;
+    private bool isBoss;
+    private int randomNumber;
+    private int modifierNumber;
 
-	// Use this for initialization
-	void Start () {
+	
+	void Start ()
+    {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime * move_speed);
+	
+	void Update ()
+    {
+        //transform.position = Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime * move_speed);
+
+
 
 
 	}
