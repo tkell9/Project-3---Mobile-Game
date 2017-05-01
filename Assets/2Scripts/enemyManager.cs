4a7b6e85b4eaaba2ba8b341a4e7571541302e17c
tkell9 <tkell9@hotmail.com>
2017-05-01 23:20:09 +1000

    no message

--- Assets/2Scripts/enemyManager.cs
+++ Assets/2Scripts/enemyManager.cs
@@ -4,29 +4,60 @@ using UnityEngine;
 
 public class enemyManager : MonoBehaviour {
 
-    private int enemyToSpawn;
+    public int enemyToSpawn; //1=basic, 2=tank, 3=spider, 4=boss
     private bool enemyReady; //If any is ready it means there is already an enemy that has spawned for combat. 
 
-    public Transform enemyPrefabNormal;
-    public Transform enemyPrefabTank;
-    public Transform enemyPrefabSpider;
-    public Transform enemyPrefabBoss;
+    public GameObject enemyPrefabNormal;
+    public GameObject enemyPrefabTank;
+    public GameObject enemyPrefabSpider;
+    public GameObject enemyPrefabBoss;
 
+    //public GameObject enemyPrefab;
     public GameObject enemySpawnPoint;
 
-    private int index1;
-    private int randomNumber;
+    //private int index1;
+    public int randomNumber;
 
 
     void Start ()
     {
-    
-		
 	}
 	
 	
 	void Update ()
     {
-		
+        if (enemyReady == false)
+        {
+            randomNumberGen();
+
+            enemyReady = true;
+        }
 	}
+
+    void randomNumberGen()
+    {
+        randomNumber = (Random.Range(0, 100));	
+    }
+
+    void whatEnemyToSpawn()
+    {
+        if(randomNumber<=55)
+        {
+            enemyToSpawn = 1;
+            Instantiate(enemyPrefabNormal, new Vector3(-32f,9.7f,0.3f),(Quaternion.identity));
+        }
+        else if(randomNumber>55 && randomNumber<=80)
+        {
+            enemyToSpawn = 2;
+        }
+        else if(randomNumber>80 && randomNumber<=95)
+        {
+            enemyToSpawn = 3;
+        }
+        else
+        {
+            enemyToSpawn = 4;
+        }
+    }
 }
+
