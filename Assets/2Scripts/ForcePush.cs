309b9964a5019fa760aa6313287f194ea0d71a2d
tkell9 <tkell9@hotmail.com>
2017-05-02 13:26:30 +1000

    no message

--- Assets/2Scripts/ForcePush.cs
+++ Assets/2Scripts/ForcePush.cs
@@ -6,17 +6,53 @@ using UnityEngine.UI;
 public class ForcePush : MonoBehaviour {
 
     public Transform playerLocation;
-    public GameObject sphere;
+    public GameObject speechBubble;
+
+    public float delay = 7f;
+    public float delaySkip = 1f;
+
+    private bool isActivated = false;
+    private bool isReady = true;
+
+    public Slider delayCounter;
+
+    public static int moveSpeed = 3;
+    public Vector3 direction = Vector3.right;
+
+    public float fakeTime;
     
 
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-
+        fakeTime = Time.fixedDeltaTime * delaySkip;
+        if (fakeTime * delaySkip >= delay)
+        {
+            isReady = true;
+        }
+        else
+        {
+            isReady = false;
+        }
 	}
+
+    public void forcePush()
+    {
+        if(isReady)
+        {
+            Instantiate(speechBubble, new Vector3(-39,3,0),Quaternion.Euler(0,-90,0));
+            
+        }
+        else
+        {
+            return;
+        }
+    }
 }
---
+++ Assets/movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+
+
+
+    public float moveSpeed = 100f;
+
+    //public float lifeTime;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+    }
+}
