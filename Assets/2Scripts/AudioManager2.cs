725686bbac1ee56a52d90d8401564c24700e647e
tkell9 <tkell9@hotmail.com>
2017-05-02 21:12:50 +1000

    no message

--- Assets/2Scripts/AudioManager2.cs
+++ Assets/2Scripts/AudioManager2.cs
@@ -5,11 +5,12 @@ using System.IO;
 using System.Linq;
 using UnityEngine.UI;
 
+
 public class AudioManager2 : MonoBehaviour
 {
     public AudioClip[] clips;
     private AudioSource audioSource;
-
+    
 
     public void Start()
     {
@@ -29,5 +30,12 @@ public class AudioManager2 : MonoBehaviour
     private AudioClip GetRandomClip()
     {
         return clips[Random.Range(0, clips.Length)];
+    }
+
+    public void changeTrack(AudioClip music)
+    {
+        audioSource.Stop();
+        audioSource.clip = music;
+        audioSource.Play();
     }
 }
